9f392106bd73e01c69191e3a4d4bf4eabdac9d6c
KazooTTT <yjwang.echo@gmail.com>
2024-11-23 21:57:41 +0800

    fix: client load

--- src/components/GiscusComment.tsx
+++ src/components/GiscusComment.tsx
@@ -1,34 +1,28 @@
+'use client'
+
 import React from 'react'
 import Giscus from '@giscus/react'
 
 const id = 'inject-comments'
 
 const GiscusComment = () => {
-	const [mounted, setMounted] = React.useState(false)
-
-	React.useEffect(() => {
-		setMounted(true)
-	}, [])
-
 	return (
 		<div id={id} className='mt-8 w-full'>
-			{mounted ? (
-				<Giscus
-					id={id}
-					repo='KazooTTT/kazoottt-blog'
-					repoId='R_kgDOMa4jRQ'
-					category='Announcements'
-					categoryId='DIC_kwDOMa4jRc4CjRFe'
-					mapping='pathname'
-					strict='0'
-					reactionsEnabled='1'
-					emitMetadata='0'
-					inputPosition='bottom'
-					theme={localStorage.getItem('theme') ?? 'preferred_color_scheme'}
-					lang='zh-CN'
-					loading='lazy'
-				/>
-			) : null}
+			<Giscus
+				id={id}
+				repo='KazooTTT/kazoottt-blog'
+				repoId='R_kgDOMa4jRQ'
+				category='Announcements'
+				categoryId='DIC_kwDOMa4jRc4CjRFe'
+				mapping='pathname'
+				strict='0'
+				reactionsEnabled='1'
+				emitMetadata='0'
+				inputPosition='bottom'
+				theme={localStorage.getItem('theme') ?? 'preferred_color_scheme'}
+				lang='zh-CN'
+				loading='lazy'
+			/>
 		</div>
 	)
 }
