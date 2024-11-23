ebcc468e7ecaec8e68e882908b978c29d9ab77d7
KazooTTT <yjwang.echo@gmail.com>
2024-11-23 22:03:59 +0800

    fix: giscus comment

--- src/components/GiscusComment.tsx
+++ src/components/GiscusComment.tsx
@@ -6,6 +6,15 @@ import Giscus from '@giscus/react'
 const id = 'inject-comments'
 
 const GiscusComment = () => {
+	const [theme, setTheme] = React.useState('preferred_color_scheme')
+
+	React.useEffect(() => {
+		const savedTheme = localStorage.getItem('theme')
+		if (savedTheme) {
+			setTheme(savedTheme)
+		}
+	}, [])
+
 	return (
 		<div id={id} className='mt-8 w-full'>
 			<Giscus
@@ -19,7 +28,7 @@ const GiscusComment = () => {
 				reactionsEnabled='1'
 				emitMetadata='0'
 				inputPosition='bottom'
-				theme={localStorage.getItem('theme') ?? 'preferred_color_scheme'}
+				theme={theme}
 				lang='zh-CN'
 				loading='lazy'
 			/>
